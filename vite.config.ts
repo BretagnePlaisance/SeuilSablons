1281d020eace2a167e3eb6671069d73f43dc547c
Valox <80895672+ImValox@users.noreply.github.com>
2025-12-15 09:12:40 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    viteSingleFile(),
   ],
   base: './',
+  build: {
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000000,
+  },
 })
